d30f58976ec92295b225d1839e786444b5416606
Sabir-itobuz <sabir@itobuz.com>
2023-03-24 18:56:51 +0530

    24 eve

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const landing = document.querySelector(".landingSection");
+
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show_nav");
+  links.classList.toggle("hidden");
+});
